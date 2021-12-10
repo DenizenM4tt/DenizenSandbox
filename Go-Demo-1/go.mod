5e69368ee037744b23670afbf58b51221fad0bcb
Jonathan <casjohn622@gmail.com>
2021-12-10 14:41:43 -0800

    go

---
+++ Go-Demo-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/DenizenM4tt/DenizenSandbox/tree/main/Go-Demo-1
+
+go 1.17
--- Go-Demo-1/main.go
+++ Go-Demo-1/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
-package mainterface{}
+import (
+	"fmt"
+
+	"github.com/DenizenM4tt/DenizenSandbox/tree/main/Go-Demo-1/mascot"
+)
+
 func main() {
-	fmt.Println(mascot.BestMascot)
+	fmt.Println(mascot.BestMascot())
 }
